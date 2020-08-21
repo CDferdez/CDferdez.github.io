5e3d3bd93d223fc81fb388bcbb98cbb3d1b31231
Christian Fernandez <cdferdez@gmail.com>
2020-08-21 16:46:48 -0500

    added contact page and style sheet

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,30 +3,29 @@ import React from 'react'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
+import '../styles/form.css'
+
 const ContactMePage = () => (
     <Layout>
         <SEO title="Contact Me" />
-        <h1>Contact Me</h1>
-        <form method="post" action="#">
-            <label>
-                Name
-                <input type="text" name="name" id="name" />
-            </label>
-            <label>
-                Email
-                <input type="email" name="email" id="email" />
-            </label>
-            <label>
-                Subject
-                <input type="text" name="subject" id="subject" />
-            </label>
-            <label>
-                Message
-                <textarea name="message" id="message" rows="5" />
-            </label>
-            <button type="submit">Send</button>
-            <input type="reset" value="Clear" />
-        </form>
+        <div className="contact-section">
+            <h1>Contact Me</h1>
+            <form className="contact-me-form" method="post" action="#">
+                <label>
+                    Name
+                    <input type="text" name="name" id="name" />
+                </label>
+                <label>
+                    Email
+                    <input type="email" name="email" id="email" />
+                </label>
+                <label>
+                    Message
+                    <textarea name="message" id="message" rows="6" />
+                </label>
+                <button type="submit">Send</button>
+            </form>    
+        </div>
     </Layout>
 )   
 
