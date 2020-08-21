95e9f36038f08c0b717b32c537b080f1f23eda87
Christian Fernandez <cdferdez@gmail.com>
2020-08-21 16:51:10 -0500

    connect contact me page to Getform

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -10,7 +10,7 @@ const ContactMePage = () => (
         <SEO title="Contact Me" />
         <div className="contact-section">
             <h1>Contact Me</h1>
-            <form className="contact-me-form" method="post" action="#">
+            <form className="contact-me-form" method="post" action="https://getform.io/f/3e4d268c-aa7b-48b6-8789-e715c28c40cb">
                 <label>
                     Name
                     <input type="text" name="name" id="name" />
